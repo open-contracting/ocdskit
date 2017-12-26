3482ba96214d50013384444e73bf8968dc2e6f29
James McKinney <james@slashpoundbang.com>
2017-12-25 19:38:49 -0500

    Add test for --pretty option

--- tests/commands/test_compile.py
+++ tests/commands/test_compile.py
@@ -8,10 +8,19 @@ from tests import read
 
 
 def test_command(monkeypatch):
-    stdin = read('release-package-1.json', 'rb') + read('release-package-2.json', 'rb')
+    stdin = read('realdata/release-package-1.json', 'rb') + read('realdata/release-package-2.json', 'rb')
 
     with patch('sys.stdin', io.TextIOWrapper(io.BytesIO(stdin))), patch('sys.stdout', new_callable=StringIO) as actual:
         monkeypatch.setattr(sys, 'argv', ['ocdskit', 'compile'])
         main()
 
-    assert actual.getvalue() == read('compiled-release-1.json') + read('compiled-release-2.json')
+    assert actual.getvalue() == read('realdata/compiled-release-1.json') + read('realdata/compiled-release-2.json')
+
+def test_command_pretty(monkeypatch):
+    stdin = read('release-package_minimal.json', 'rb')
+
+    with patch('sys.stdin', io.TextIOWrapper(io.BytesIO(stdin))), patch('sys.stdout', new_callable=StringIO) as actual:
+        monkeypatch.setattr(sys, 'argv', ['ocdskit', '--pretty', 'compile'])
+        main()
+
+    assert actual.getvalue() == read('compile_pretty_minimal.json')
