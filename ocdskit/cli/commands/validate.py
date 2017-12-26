81d45b6b9d60478e70c5b3102e80875fc67c57c4
James McKinney <james@slashpoundbang.com>
2017-12-25 20:36:21 -0500

    Allow local file path for --schema, and capture JSON errors

--- ocdskit/cli/commands/validate.py
+++ ocdskit/cli/commands/validate.py
@@ -1,10 +1,12 @@
 import json
+from urllib.parse import urlparse
 
 import requests
 from jsonschema import FormatChecker
 from jsonschema.validators import Draft4Validator as validator
 
 from .base import BaseCommand
+from ocdskit.exceptions import CommandError
 
 
 class Command(BaseCommand):
@@ -16,9 +18,17 @@ class Command(BaseCommand):
                           default='http://standard.open-contracting.org/latest/en/release-package-schema.json')
 
     def handle(self):
-        schema = requests.get(self.args.schema).json()
+        components = urlparse(self.args.schema)
+        if components.scheme == 'file':
+            with open(self.args.schema[7:]) as f:
+                schema = json.loads(f.read())
+        else:
+            schema = requests.get(self.args.schema).json()
 
         for i, line in enumerate(self.buffer()):
-            data = json.loads(line)
-            for error in validator(schema, format_checker=FormatChecker()).iter_errors(data):
-                print('item {}: {} ({})'.format(i, error.message, '/'.join(error.absolute_schema_path)))
+            try:
+                data = json.loads(line)
+                for error in validator(schema, format_checker=FormatChecker()).iter_errors(data):
+                    print('item {}: {} ({})'.format(i, error.message, '/'.join(error.absolute_schema_path)))
+            except json.decoder.JSONDecodeError as e:
+                raise CommandError('item {}: JSON error: {}'.format(i, e))
