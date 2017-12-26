0ccdb93bf1b428fb4c614af51b00cfc5ef9bb746
James McKinney <james@slashpoundbang.com>
2017-12-25 22:05:03 -0500

    Remove assumption from compile command

--- ocdskit/cli/commands/compile.py
+++ ocdskit/cli/commands/compile.py
@@ -1,3 +1,4 @@
+import collections
 import json
 
 import ocdsmerge
@@ -7,11 +8,16 @@ from .base import BaseCommand
 
 class Command(BaseCommand):
     name = 'compile'
-    help = 'reads a release package from standard input, merges its releases, and prints the compiled release'
+    help = 'reads release packages from standard input, merges the releases by OCID, and prints the compiled releases'
 
     def handle(self):
-        # This assumes the releases in the release package are for one contracting process.
+        releases_by_ocid = collections.defaultdict(list)
+
         for line in self.buffer():
             release_package = json.loads(line)
-            compiled_release = ocdsmerge.merge(release_package['releases'])
+            for release in release_package['releases']:
+                releases_by_ocid[release['ocid']].append(release)
+
+        for releases in releases_by_ocid.values():
+            compiled_release = ocdsmerge.merge(releases)
             self.print(compiled_release)
