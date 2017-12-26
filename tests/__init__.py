6cda10e91e8f03c32b1bc23188d603ebea8551a5
James McKinney <james@slashpoundbang.com>
2017-12-25 23:30:23 -0500

    Add simple mapping-sheet test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
 import os.path
 
 
-def read(filename, mode='r', encoding=None):
+def read(filename, mode='rt', encoding=None):
     with open(os.path.join('tests', 'fixtures', filename), mode, encoding=encoding) as f:
         return f.read()
