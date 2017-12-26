6db1730c3d80a016fbc3cb46e01959e47d2709e4
James McKinney <james@slashpoundbang.com>
2017-12-25 19:17:53 -0500

    Increase coverage of combine-release-packages

--- tests/commands/test_combine_release_packages.py
+++ tests/commands/test_combine_release_packages.py
@@ -8,10 +8,21 @@ from tests import read
 
 
 def test_command(monkeypatch):
-    stdin = read('release-package-1.json', 'rb') + read('release-package-2.json', 'rb')
+    stdin = read('release-package-minimal.json', 'rb') + \
+            read('release-package-maximal.json', 'rb') + \
+            read('release-package-extensions.json', 'rb')
 
     with patch('sys.stdin', io.TextIOWrapper(io.BytesIO(stdin))), patch('sys.stdout', new_callable=StringIO) as actual:
         monkeypatch.setattr(sys, 'argv', ['ocdskit', 'combine-release-packages'])
         main()
 
-    assert actual.getvalue() == read('release-package-1-2.json')
+    assert actual.getvalue() == read('combine-minimal-maximal-extensions.json')
+
+def test_command_no_extensions(monkeypatch):
+    stdin = read('release-package-minimal.json', 'rb')
+
+    with patch('sys.stdin', io.TextIOWrapper(io.BytesIO(stdin))), patch('sys.stdout', new_callable=StringIO) as actual:
+        monkeypatch.setattr(sys, 'argv', ['ocdskit', 'combine-release-packages'])
+        main()
+
+    assert actual.getvalue() == read('combine-minimal.json')
