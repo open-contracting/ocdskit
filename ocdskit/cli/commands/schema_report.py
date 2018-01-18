6954a6e8cb3224ae070820f2e65cb45ade83db56
James McKinney <james@slashpoundbang.com>
2018-01-18 14:28:03 -0500

    Add schema-report command

---
+++ ocdskit/cli/commands/schema_report.py
@@ -0,0 +1,46 @@
+import json
+
+from .base import BaseCommand
+
+
+class Command(BaseCommand):
+    name = 'schema-report'
+    help = 'Reports details of a JSON Schema'
+
+    def add_arguments(self):
+        self.add_argument('file', help='JSON Schema file')
+
+    def handle(self):
+        def recurse(data, codelists):
+            if isinstance(data, list):
+                for item in data:
+                    recurse(item, codelists)
+            elif isinstance(data, dict):
+                if 'codelist' in data:
+                    codelist = data['codelist']
+                    # If the CSV file is used for open and closed codelists, treat it as closed.
+                    if codelist not in codelists:
+                        codelists[codelist] = data['openCodelist']
+                    elif not data['openCodelist']:
+                        codelists[codelist] = data['openCodelist']
+                else:
+                    for value in data.values():
+                        recurse(value, codelists)
+
+        with open(self.args.file) as f:
+            data = json.load(f)
+
+        codelists = {}
+        recurse(data, codelists)
+
+        partitioned_codelists = {
+            True: set(),
+            False: set(),
+        }
+
+        for codelist, open_codelist in codelists.items():
+            partitioned_codelists[open_codelist].add(codelist)
+
+        for open_codelist, codelists in partitioned_codelists.items():
+            if codelists:
+                print('openCodelist: {}\n{}\n'.format(open_codelist, '\n'.join(sorted(codelists))))
