416600fe06f8486c474b53710c44fbb5cafaf642
Ben Webb <bjwebb67@googlemail.com>
2018-07-12 11:45:24 +0100

    [#42] Add seperate rows for the reference, and the object referred to

--- ocdskit/cli/commands/mapping_sheet.py
+++ ocdskit/cli/commands/mapping_sheet.py
@@ -33,74 +33,86 @@ class Command(BaseCommand):
         def display_links(links):
             return ', '.join(links.values())
 
+        def make_row(path, field, schema, deprecated, required_fields, is_reference=False):
+            row = {'path': path+field, 'deprecated': deprecated}
+
+            section = row['path'].split('/')[0] if '/' in row['path'] else ''
+
+            row['section'] = section
+
+            row['title'] = schema['title'] if 'title' in schema else field + '*'
+
+            if 'description' in schema:
+                links = find_md_links(schema['description'])
+                row['description'] = remove_links(schema['description'], links)
+                row['links'] = display_links(links)
+
+            required = False
+
+            # Type
+            if 'type' in schema:
+                # This checks whether this field is **implicity required**
+                type_ = copy.copy(schema['type'])
+                if 'null' in type_:
+                    type_.remove('null')
+                else:
+                    required = 'string' in type_ or 'integer' in type_
+
+                if type(type_) in (tuple, list):
+                    row['type'] = ', '.join(type_)
+                else:
+                    row['type'] = type_
+            elif is_reference:
+                row['type'] = 'reference'
+            else:
+                row['type'] = 'unknown'
+
+            # This checks whether this field is **explicitly required**
+            if field in required_fields:
+                required = True
+
+            maxn = 'n' if row['type'] == 'array' else '1'
+            minn = '1' if required else '0'
+            row['range'] = minn + '..' + maxn
+
+            # Format or restrictions
+            if 'format' in schema:
+                row['values'] = schema['format']
+            elif 'enum' in schema:
+                values = copy.copy(schema['enum'])
+                if None in values:
+                    values.remove(None)
+                row['values'] = 'Codelist: ' + ', '.join(values)
+            else:
+                row['values'] = ''
+
+            # Check for deprecation
+            if 'deprecated' in schema:
+                row['deprecated'] = schema['deprecated'].get('deprecatedVersion', '')
+                row['deprecationNotes'] = schema['deprecated'].get('description', '')
+
+            return row
+
         def display_properties(schema, path='', section='', deprecated=''):
             obj = schema['properties']
             required_fields = schema['required'] if 'required' in schema else []
             rows = []
             for field in obj:
-                row = {'path': path + field, 'deprecated': deprecated}
-
-                section = row['path'].split('/')[0] if '/' in row['path'] else ''
-
-                row['section'] = section
-
-                # If there was a reference here, prefer the values from that
+                # If there was a reference, add an extra row for that
                 if hasattr(obj[field], '__reference__'):
-                    obj[field].update(obj[field].__reference__)
-
-                row['title'] = obj[field]['title'] if 'title' in obj[field] else field + '*'
-
-                if 'description' in obj[field]:
-                    links = find_md_links(obj[field]['description'])
-                    row['description'] = remove_links(obj[field]['description'], links)
-                    row['links'] = display_links(links)
-
-                # Type
-                if 'type' in obj[field]:
-                    # ToDo: Add checking of the required array also.
-                    # This checks whether this field is **implicity required**
-                    type_ = copy.copy(obj[field]['type'])
-                    if 'null' in type_:
-                        type_.remove('null')
-                        required = False
-                    else:
-                        required = 'string' in type_ or 'integer' in type_
-
-                    if type(type_) in (tuple, list):
-                        row['type'] = ', '.join(type_)
-                    else:
-                        row['type'] = type_
+                    reference_row = make_row(
+                        path, field, obj[field].__reference__, deprecated, required_fields, is_reference=True)
+                    rows.append(reference_row)
                 else:
-                    row['type'] = 'unknown'
+                    reference_row = {}
 
-                # Required field
-                if field in required_fields:
-                    required = True
-
-                maxn = 'n' if row['type'] == 'array' else '1'
-                minn = '1' if required else '0'
-                row['range'] = minn + '..' + maxn
-
-                # Format or restrictions
-                if 'format' in obj[field]:
-                    row['values'] = obj[field]['format']
-                elif 'enum' in obj[field]:
-                    values = copy.copy(obj[field]['enum'])
-                    if None in values:
-                        values.remove(None)
-                    row['values'] = 'Codelist: ' + ', '.join(values)
-                else:
-                    row['values'] = ''
-
-                # Check for deprecation
-                if 'deprecated' in obj[field]:
-                    row['deprecated'] = obj[field]['deprecated'].get('deprecatedVersion', '')
-                    row['deprecationNotes'] = obj[field]['deprecated'].get('description', '')
-
+                row = make_row(path, field, obj[field], deprecated, required_fields)
                 rows.append(row)
 
+                children_deprecated = reference_row.get('deprecated') or reference_row.get('deprecated')
+
                 if 'properties' in obj[field]:
-                    rows = rows + display_properties(obj[field], path + field + '/', section, row['deprecated'])
+                    rows = rows + display_properties(obj[field], path + field + '/', section, children_deprecated)
 
                 if 'items' in obj[field]:
                     if 'properties' in obj[field]['items']:
