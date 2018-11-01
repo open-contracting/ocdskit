6f658a042f08cb52368fac451b46a117f947f9e5
James McKinney <james@slashpoundbang.com>
2018-11-01 15:20:56 -0400

    flake8

--- tests/commands/test_release_package.py
+++ tests/commands/test_release_package.py
@@ -20,8 +20,8 @@ def test_command_extensions(monkeypatch):
     stdin = read('release_minimal1.json', 'rb') + read('release_minimal2.json', 'rb')
 
     with patch('sys.stdin', TextIOWrapper(BytesIO(stdin))), patch('sys.stdout', new_callable=StringIO) as actual:
-        monkeypatch.setattr(sys, 'argv', ['ocdskit', 'release-package',
-            'http://example.com/a/extension.json', 'http://example.com/b/extension.json'])
+        monkeypatch.setattr(sys, 'argv', ['ocdskit', 'release-package', 'http://example.com/a/extension.json',
+                                          'http://example.com/b/extension.json'])
         main()
 
     assert actual.getvalue() == read('release-package_minimal1-minimal2-extensions.json')
