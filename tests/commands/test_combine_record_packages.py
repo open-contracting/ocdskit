0af3ca9ea919b83d84777915417ddc114a6375fb
James McKinney <james@slashpoundbang.com>
2019-01-04 17:18:37 -0500

    Add and update tests for indent, combine-record-packages, combine-release-packages

--- tests/commands/test_combine_record_packages.py
+++ tests/commands/test_combine_record_packages.py
@@ -1,3 +1,4 @@
+import json
 import sys
 from io import BytesIO, StringIO, TextIOWrapper
 from unittest.mock import patch
@@ -26,3 +27,16 @@ def test_command_no_extensions(monkeypatch):
         main()
 
     assert actual.getvalue() == read('combine-record-packages_minimal.json')
+
+
+def test_command_uri_published_date(monkeypatch):
+    stdin = read('record-package_minimal.json', 'rb')
+
+    with patch('sys.stdin', TextIOWrapper(BytesIO(stdin))), patch('sys.stdout', new_callable=StringIO) as actual:
+        monkeypatch.setattr(sys, 'argv', ['ocdskit', 'combine-record-packages', '--uri', 'http://example.com/x.json',
+                                          '--published-date', '2010-01-01T00:00:00Z'])
+        main()
+
+    package = json.loads(actual.getvalue())
+    assert package['uri'] == 'http://example.com/x.json'
+    assert package['publishedDate'] == '2010-01-01T00:00:00Z'
