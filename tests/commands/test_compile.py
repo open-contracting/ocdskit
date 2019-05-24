5ee140b1f6cba7e3a156a1c0636b794f52222856
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-05-24 17:31:13 -0400

    Add tests for publisher options

--- tests/commands/test_compile.py
+++ tests/commands/test_compile.py
@@ -52,6 +52,17 @@ def test_command_package_uri_published_date(monkeypatch):
     assert package['publishedDate'] == '2010-01-01T00:00:00Z'
 
 
+def test_command_package_publisher(monkeypatch):
+    stdin = read('release-package_minimal.json', 'rb')
+
+    with patch('sys.stdin', TextIOWrapper(BytesIO(stdin))), patch('sys.stdout', new_callable=StringIO) as actual:
+        monkeypatch.setattr(sys, 'argv', ['ocdskit', 'compile', '--package', '--publisher-name', 'Acme Inc.'])
+        main()
+
+    package = json.loads(actual.getvalue())
+    assert package['publisher']['name'] == 'Acme Inc.'
+
+
 def test_command_package_linked_releases(monkeypatch):
     stdin = read('realdata/release-package-1.json', 'rb') + read('realdata/release-package-2.json', 'rb')
 
