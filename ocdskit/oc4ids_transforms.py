3c67b17a5e0d44a68b71293de7426134c407bfb5
David Raznick <kindly@gmail.com>
2020-01-15 11:12:22 +0000

    Initial transforms

---
+++ ocdskit/oc4ids_transforms.py
@@ -0,0 +1,117 @@
+import copy
+
+import jsonpointer
+
+from ocdskit.combine import merge
+
+
+class InitialTransformState:
+
+    def __init__(self, config, releases, project_id=None, compiled_releases=None, output=None):
+        self.config = config
+        self.releases = releases
+        self.project_id = project_id
+        if not compiled_releases:
+            compiled_releases = [compiled_release for compiled_release in merge(self.releases)]
+        self.compiled_releases = compiled_releases
+        self.output = output or {}
+        if project_id and 'id' not in self.output:
+            self.output['id'] = project_id
+        self.success = True
+
+
+class BaseTransform:
+
+    def __init__(self, last_transforms):
+        last_transform = last_transforms[-1]
+        self.last_transforms = last_transforms
+        self.config = last_transform.config
+        self.releases = last_transform.releases
+        self.compiled_releases = last_transform.compiled_releases
+        self.output = last_transform.output
+        self.last_transform_success = last_transform.success
+        self.success = False
+
+        self.run()
+
+    def run(self):
+        pass
+
+    def copy_party_by_role(self, role, new_roles=None):
+
+        for compiled_release in self.compiled_releases:
+            parties = compiled_release.get('parties', [])
+            if not isinstance(parties, list):
+                continue
+            for party in compiled_release.get('parties', []):
+                if role in party.get('roles', []):
+                    output_parties = self.output.get('parties', [])  
+                    if not output_parties:
+                        self.output['parties'] = output_parties
+                    output_party = copy.deepcopy(party)
+                    if new_roles:
+                        output_roles = output_party.get('roles', [])
+                        output_roles.extend(new_roles)
+                        output_party['roles'] = output_roles
+                    output_parties.append(output_party)
+                    self.success = True
+
+
+class PublicAuthorityRole(BaseTransform):
+    def run(self):
+        self.copy_party_by_role('publicAuthority')
+
+
+class BuyerRole(BaseTransform):
+    def run(self):
+        if self.config.get('copy_buyer_role'):
+            self.copy_party_by_role('buyer', ['publicAuthority'])
+        else:
+            self.success = True
+
+
+class Sector(BaseTransform):
+    def run(self):
+        for compiled_release in self.compiled_releases:
+            sector = jsonpointer.resolve_pointer(compiled_release, '/planning/project/sector', None)
+            if sector:
+                self.output['sector'] = sector
+                self.success = True
+                break
+
+
+class AdditionalClassifications(BaseTransform):
+    def run(self):
+        for compiled_release in self.compiled_releases:
+            additionalClassifications = jsonpointer.resolve_pointer(compiled_release, '/planning/project/additionalClassifications', None)
+            if additionalClassifications:
+                self.output['additionalClassifications'] = additionalClassifications
+                self.success = True
+                break
+
+
+class Title(BaseTransform):
+    def run(self):
+        for compiled_release in self.compiled_releases:
+            title = jsonpointer.resolve_pointer(compiled_release, '/planning/project/title', None)
+            if title:
+                self.output['title'] = title
+                self.success = True
+                break
+
+
+class TitleFromTender(BaseTransform):
+    def run(self):
+        if not self.config.get('use_tender_title'):
+            self.success = True
+            return
+        if self.last_transforms[-1].success:
+            self.success = True
+            return
+
+        for compiled_release in self.compiled_releases:
+            title = jsonpointer.resolve_pointer(compiled_release, '/tender/title', None)
+            if title:
+                self.output['title'] = title
+                self.success = True
+                break
---
+++ tests/test_oc4ids_tranforms.py
@@ -0,0 +1,152 @@
+import json
+import copy
+
+import ocdskit.oc4ids_transforms as transforms
+from tests import read
+
+
+def test_initial_tranform_state():
+    releases = json.loads(read('release-package_additional-contact-points.json'))['releases']
+    initial_transform = transforms.InitialTransformState({}, releases, '1', compiled_releases=None)
+    assert len(initial_transform.compiled_releases) == 1
+
+
+def test_public_authority_role():
+    releases = [
+        {"ocid":"ocds-213czf-1",
+         "id":"1",
+         "tag":"planning",
+         "date":"2001-02-03T04:05:06Z",
+         "parties":[{"id": "1", "roles": ["publicAuthority"]}, 
+                    {"id": "2", "roles": ["publicAuthority"]}]
+        }
+    ]
+
+    initial_transform = transforms.InitialTransformState({}, releases, '1', compiled_releases=None)
+    transform = transforms.PublicAuthorityRole([initial_transform])
+    assert transform.success
+    assert transform.output['parties'] == releases[0]['parties']
+
+    releases = [
+        {"ocid":"ocds-213czf-1",
+         "id":"1",
+         "tag":"planning",
+         "date":"2001-02-03T04:05:06Z",
+         "parties":[{"id": "1", "roles": ["publicAuthority"]}, 
+                    {"id": "2", "roles": ["buyer"]}]
+        }
+    ]
+
+    initial_transform = transforms.InitialTransformState({}, releases, '1', compiled_releases=None)
+    transform = transforms.PublicAuthorityRole([initial_transform])
+    assert transform.success
+    assert len(transform.output['parties']) == 1
+
+
+def test_buyer_role():
+    releases = [
+        {"ocid":"ocds-213czf-1",
+         "id":"1",
+         "tag":"planning",
+         "date":"2001-02-03T04:05:06Z",
+         "parties":[{"id": "1", "roles": ["buyer"]}]
+        }
+    ]
+
+    initial_transform = transforms.InitialTransformState({}, copy.deepcopy(releases), '1', compiled_releases=None)
+    transform = transforms.BuyerRole([initial_transform])
+
+    # No config to say to convert buyers
+    assert 'parties' not in transform.output
+    assert transform.success
+
+    initial_transform = transforms.InitialTransformState({'copy_buyer_role': True},
+                                                         copy.deepcopy(releases), 
+                                                         '1', 
+                                                         compiled_releases=None)
+    transform = transforms.BuyerRole([initial_transform])
+
+    assert transform.success
+    assert "publicAuthority" in transform.output['parties'][0]['roles']
+    assert "buyer" in transform.output['parties'][0]['roles']
+
+
+def test_sector():
+    releases = [
+        {"ocid":"ocds-213czf-1",
+         "id":"1",
+         "tag":"planning",
+         "date":"2001-02-03T04:05:06Z",
+         "planning": {"project": {"sector": "a"}},
+        }
+    ]
+
+    initial_transform = transforms.InitialTransformState({}, copy.deepcopy(releases), '1', compiled_releases=None)
+    transform = transforms.Sector([initial_transform])
+    assert transform.success
+    assert transform.output['sector'] == 'a'
+
+
+def test_additional_classifications():
+    releases = [
+        {"ocid":"ocds-213czf-1",
+         "id":"1",
+         "tag":"planning",
+         "date":"2001-02-03T04:05:06Z",
+         "planning": {"project": {"additionalClassifications": [{"scheme": "a", "id": "1"}]}},
+        }
+    ]
+
+    initial_transform = transforms.InitialTransformState({}, copy.deepcopy(releases), '1', compiled_releases=None)
+    transform = transforms.AdditionalClassifications([initial_transform])
+    assert transform.success
+    assert transform.output['additionalClassifications'] == [{"scheme": "a", "id": "1"}]
+
+
+def test_title():
+    releases = [
+        {"ocid":"ocds-213czf-1",
+         "id":"1",
+         "tag":"planning",
+         "date":"2001-02-03T04:05:06Z",
+         "planning": {"project": {"title": "a title"}},
+        }
+    ]
+
+    initial_transform = transforms.InitialTransformState({}, copy.deepcopy(releases), '1', compiled_releases=None)
+    transform = transforms.Title([initial_transform])
+    assert transform.success
+    assert transform.output['title'] == "a title"
+
+
+def test_title_from_tender():
+    releases = [
+        {"ocid":"ocds-213czf-1",
+         "id":"1",
+         "tag":"planning",
+         "date":"2001-02-03T04:05:06Z",
+         "tender": {"title": "a title"},
+        }
+    ]
+
+    initial_transform = transforms.InitialTransformState({'use_tender_title': True}, copy.deepcopy(releases), '1', compiled_releases=None)
+    title_transform = transforms.Title([initial_transform])
+    transform = transforms.TitleFromTender([initial_transform, title_transform])
+    assert transform.success
+    assert transform.output['title'] == "a title"
+
+    releases = [
+        {"ocid":"ocds-213czf-1",
+         "id":"1",
+         "tag":"planning",
+         "date":"2001-02-03T04:05:06Z",
+         "planning": {"project": {"title": "a title"}},
+         "tender": {"title": "a non used title"},
+        }
+    ]
+
+    initial_transform = transforms.InitialTransformState({'use_tender_title': True}, copy.deepcopy(releases), '1', compiled_releases=None)
+    title_transform = transforms.Title([initial_transform])
+    transform = transforms.TitleFromTender([initial_transform, title_transform])
+    assert transform.success
+    assert transform.output['title'] == "a title"
