a7592e860916fac019278215fa1ccd1c9fd97eba
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-03-19 22:51:16 -0400

    Iterate the version number. Update changelog.

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ copyright = '2017, Open Contracting Partnership'
 author = 'Open Contracting Partnership'
 
 # The short X.Y version
-version = '0.2.3'
+version = '0.2.4'
 # The full version, including alpha/beta/rc tags
 release = version
 
