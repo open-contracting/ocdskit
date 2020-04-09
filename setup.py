988856d367c52c2ea81ef7b0ecd493ecfcd27796
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-04-09 16:17:56 -0400

    Add classifiers

--- setup.py
+++ setup.py
@@ -43,7 +43,10 @@ setup(
     },
     classifiers=[
         'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     entry_points={
         'console_scripts': [
