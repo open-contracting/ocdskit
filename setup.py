957befa875283f18a87d050ebe68056ed55c9bb0
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-04-29 12:22:52 -0400

    Iterate the version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='ocdskit',
-    version='0.2.7',
+    version='0.2.8',
     author='Open Contracting Partnership',
     author_email='data@open-contracting.org',
     url='https://github.com/open-contracting/ocdskit',
