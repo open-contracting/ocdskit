ea923e3cdc4712b4e87e2a2f4cc0bbbbc971be7e
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-08-19 20:46:28 -0400

    Iterate the version number

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ copyright = '2017, Open Contracting Partnership'
 author = 'Open Contracting Partnership'
 
 # The short X.Y version
-version = '0.2.11'
+version = '0.2.12'
 # The full version, including alpha/beta/rc tags
 release = version
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='ocdskit',
-    version='0.2.11',
+    version='0.2.12',
     author='Open Contracting Partnership',
     author_email='data@open-contracting.org',
     url='https://github.com/open-contracting/ocdskit',
