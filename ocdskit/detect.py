1b7b27d4235eee1d393fc9290a3814951e562a7a
jakubkrafka <jakub@krafka.net>
2020-12-14 15:05:21 +0100

    Update ocdskit/detect.py
    Co-authored-by: James McKinney <26463+jpmckinney@users.noreply.github.com>

--- ocdskit/detect.py
+++ ocdskit/detect.py
@@ -7,7 +7,9 @@ logger = logging.getLogger('ocdskit')
 
 def detect_format(path, root_path=''):
     """
-    Returns the file format with additional info whether the items are in array and/or concatenated.
+    Returns the format of OCDS data, and whether the OCDS data is concatenated or in an array.
+    
+    If the OCDS data is concatenated or in an array, assumes that all items have the same format as the first item.
 
     :param str path: path to tested file
     :param str root_path: ``json path`` within the file, where the tested items will be searched for
