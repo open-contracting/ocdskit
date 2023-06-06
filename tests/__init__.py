1f9970e7d08eb5208e487ae691dd67c1126a8b7c
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-06-06 12:18:20 -0400

    test: Set encoding when opening files for Windows

--- tests/__init__.py
+++ tests/__init__.py
@@ -14,8 +14,10 @@ def path(filename):
     return os.path.join('tests', 'fixtures', filename)
 
 
-def read(filename, mode='rt', encoding=None, **kwargs):
-    with open(path(filename), mode, encoding=encoding, **kwargs) as f:
+def read(filename, mode='rt', **kwargs):
+    if 'b' not in mode:
+        kwargs['encoding'] = 'utf-8'
+    with open(path(filename), mode, **kwargs) as f:
         return f.read()
 
 
