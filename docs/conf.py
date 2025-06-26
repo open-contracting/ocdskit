f808d99fc244333544f2cf1919d81b6bde6bb6b3
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-06-26 16:08:56 -0400

    docs: Use pathlib with sys.path

--- docs/conf.py
+++ docs/conf.py
@@ -3,10 +3,10 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import os
 import sys
+from pathlib import Path
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, str(Path(__file__).parent.parent))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
